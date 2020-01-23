0a4bd5e4bd578133abc53f2a52729d7641fa4b8f
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-01-22 21:19:16 -0300

    Alteração no controller

--- project/src/main/java/com/esigproject/project/controller/ProjectEsigController.java
+++ project/src/main/java/com/esigproject/project/controller/ProjectEsigController.java
@@ -1,13 +1,43 @@
 package com.esigproject.project.controller;
 
+import com.esigproject.project.model.Item;
+import com.esigproject.project.services.ItemService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller
+
+@Controller("/")
 public class ProjectEsigController {
 
-    @RequestMapping("/home")
-    public String index() {
-        return "home/index";
+    @Autowired
+    private ItemService itemService; 
+
+    @RequestMapping("home")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("item", new Item());
+        mv.setViewName("home/index");
+        return mv;
+    }
+
+    @PostMapping("salvar")
+    public void salvar(@RequestBody Item item) {
+        itemService.save(item);
+    }
+    
+    @DeleteMapping
+    public void delete(@RequestBody Item item) {
+        itemService.save(item);
+    }
+
+    @PostMapping("update")
+    public void update(@RequestBody Item item) {
+        itemService.save(item);
     }
 }
