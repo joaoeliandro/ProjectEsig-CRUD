05941952d43cce4feec7370b1c4eb22bde0cb813
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-01-23 09:53:30 -0300

    Atualização nos rest

--- project/src/main/java/com/esigproject/project/controller/ProjectEsigController.java
+++ project/src/main/java/com/esigproject/project/controller/ProjectEsigController.java
@@ -12,12 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/home")
@@ -40,25 +37,20 @@ public class ProjectEsigController {
     }
 
     @PostMapping("/item")
-    public ModelAndView salvar(@Valid Item item) throws Exception{
+    public ModelAndView salvar(@Valid Item item) throws Exception {
         itemService.save(item);
         return new ModelAndView("redirect:/home");
     }
 
-    @DeleteMapping("/item/{id}")
-    public ResponseEntity<Item> delete(@PathVariable(value = "id") Long id) {
+    @RequestMapping("/delete")
+    public ModelAndView delete(Long id) {
         Item item = itemService.findById(id);
-        if (item == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        itemService.deleteById(id);
-
-        return ResponseEntity.ok().build();
+        itemService.deleteById(item.getId());
+        return new ModelAndView("redirect:/home");
     }
 
     @PutMapping("/item/{id}")
-    public ResponseEntity<Item> update(@PathVariable(value = "id") Long id, @Valid @RequestBody Item item) {
+    public ResponseEntity<Item> update(@PathVariable(value = "id") Long id, @Valid Item item) {
         Item itemNovo = itemService.findById(id);
 
         if (itemNovo == null) {
@@ -69,7 +61,7 @@ public class ProjectEsigController {
         itemNovo.setDestinatario(item.getDestinatario());
         itemNovo.setRemetente(item.getRemetente());
         itemNovo.setEmail(item.getEmail());
-
+        
         itemService.save(itemNovo);
         return ResponseEntity.ok().body(itemNovo);
     }
