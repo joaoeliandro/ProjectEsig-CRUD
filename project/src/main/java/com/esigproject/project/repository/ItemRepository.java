97c11b6d60e8e8a53e45a3b62b69ff0eb707fa33
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-01-22 21:41:50 -0300

    Repositorio estendendo classe JPA

--- project/src/main/java/com/esigproject/project/repository/ItemRepository.java
+++ project/src/main/java/com/esigproject/project/repository/ItemRepository.java
@@ -2,9 +2,10 @@ package com.esigproject.project.repository;
 
 import com.esigproject.project.model.Item;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ItemRepository extends Item {
-
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findById(long id);
 }
