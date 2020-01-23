9b7c93f8bcf166fd924ecff5ab7a9467837863d2
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-01-22 21:42:39 -0300

    Funções do service injetando dependecias do repositorio

--- project/src/main/java/com/esigproject/project/services/ItemService.java
+++ project/src/main/java/com/esigproject/project/services/ItemService.java
@@ -1,5 +1,7 @@
 package com.esigproject.project.services;
 
+import java.util.List;
+
 import com.esigproject.project.model.Item;
 import com.esigproject.project.repository.ItemRepository;
 
@@ -8,12 +10,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService {
-    
+
     @Autowired
     private ItemRepository itemRepository;
-    
-    public void save(Item item){
+
+    public void save(Item item) {
         itemRepository.save(item);
+    }
+
+    public Item findById(long id) {
+        return itemRepository.findById(id);
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+
+    public void deleteById(long id) {
+        itemRepository.deleteById(id);
     }
 
 }
