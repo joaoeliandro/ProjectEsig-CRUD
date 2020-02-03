38a1f9d5c851ccd9c72e8e688d137f22ff2a8ba8
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-02-03 17:08:31 -0300

    Retorno do script.js

--- project/src/main/resources/static/script.js
+++ project/src/main/resources/static/script.js
@@ -1,23 +1,26 @@
+var id;
+
 $(document).ready(() => {
     let table = $('#table').DataTable({
         "info": false,
         "paging": false
     });
+});
 
-    $('#table tbody').on('click', '#btnEdit', () => {
-        if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
+$("#btnAtt").click(() => {
+    $.ajax({
+        url: "/item/" + id,
+        type: "PUT",
+        success: (response) => {
+            console.log(response);
+            alert("Atualizado");
+            // $("#btnAtt").prop("disabled", true);
         }
-        else {
-            table.$('tr.selected').removeClass('selected');
-            $(this).addClass('selected');
-        }
-
-        let data = table.row({ selected: true }).data();
-        console.log(data)
-        $('#itemNome').val(data[1]);
-        $('#itemDest').val(data[2]);
-        $('#itemRemet').val(data[3]);
-        $('#itemEmailRemet').val(data[4]);
     });
 });
+
+function editar(idClicked) {
+    id = idClicked;
+    console.log(id);
+    $("#btnAtt").prop("disabled", false);
+}
