149a65ecd9175c84a5906c778813b735993f52ba
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-02-03 17:21:42 -0300

    Teste DataConfig

--- project/src/main/java/com/esigproject/project/config/DataConfigurationDev.java
+++ project/src/main/java/com/esigproject/project/config/DataConfigurationDev.java
@@ -1,36 +1,36 @@
-package com.esigproject.project.config;
+// package com.esigproject.project.config;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+// import org.springframework.orm.jpa.JpaVendorAdapter;
+// import org.springframework.orm.jpa.vendor.Database;
+// import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-@Configuration
-@Profile("dev")
-public class DataConfigurationDev {
+// @Configuration
+// @Profile("dev")
+// public class DataConfigurationDev {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/projectesig?useSSL=false&useTimezone=true&serverTimezone=UTC");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        return dataSource;
-    }
+//     @Bean
+//     public DataSource dataSource() {
+//         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//         dataSource.setUsername("root");
+//         dataSource.setPassword("");
+//         dataSource.setUrl("jdbc:mysql://localhost:3306/projectesig?useSSL=false&useTimezone=true&serverTimezone=UTC");
+//         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//         return dataSource;
+//     }
 
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
-}
+//     @Bean
+//     public JpaVendorAdapter jpaVendorAdapter() {
+//         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//         adapter.setDatabase(Database.MYSQL);
+//         adapter.setGenerateDdl(true);
+//         adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+//         adapter.setPrepareConnection(true);
+//         return adapter;
+//     }
+// }
